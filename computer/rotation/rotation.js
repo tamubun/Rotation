ebcfc8f1f24d07c82daf5493b865e5d55539b1ca
tamubun <sutebubon@gmail.com>
2013-11-01 16:49:46 +0900

    回り始めた

--- computer/rotation/rotation.js
+++ computer/rotation/rotation.js
@@ -1,6 +1,18 @@
 'use strict';
 var camera, scene, renderer, controls;
 
+var radius, height, theta, mom;
+var cylinder;
+
+function newSettings() {
+  radius = Number($('#radius').val());
+  height = Number($('#height').val());
+  theta = Number($('#theta').val());
+  mom = Number($('#mom').val());
+  cylinder.scale.set(radius * 50, height * 50, radius * 50);
+  cylinder.rotation.set(0,0,Math.PI/180.0 * theta);
+}
+
 function init() {
   var arena = $('#arena');
   scene = new THREE.Scene();
@@ -8,7 +20,7 @@ function init() {
     45, arena.innerWidth() / arena.innerHeight(), 1, 2000);
   camera.position.set(447, 400, -665);
   camera.lookAt( scene.position );
-  controls = new THREE.TrackballControls(camera);
+  controls = new THREE.TrackballControls(camera, arena[0]);
   controls.rotateSpeed = 1.0;
   controls.zoomSpeed = 1.2;
   controls.panSpeed = 0.8;
@@ -26,10 +38,11 @@ function init() {
   var material = new THREE.MeshLambertMaterial(
     { ambient: 0xbbbbbb, color: 0x335577 });
 
-  var object = new THREE.Mesh(
-    new THREE.SphereGeometry(75, 20, 10), material );
-  object.position.set( -400, 0, 200 );
-  scene.add( object );
+  cylinder = new THREE.Mesh(
+    new THREE.CylinderGeometry(1, 1, 1, 20, 1), material );
+  cylinder.position.set( 0, 0, 0 );
+  newSettings();
+  scene.add( cylinder );
 
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(arena.innerWidth(), arena.innerHeight());
@@ -37,12 +50,15 @@ function init() {
 }
 
 function animate() {
+  var timer = Date.now() * 0.0002;
   requestAnimationFrame(animate);
+  cylinder.rotation.y = timer * mom;
   controls.update();
   renderer.render(scene, camera);
 }
 
 $(function() {
+  $('#settings input').change(newSettings);
   init();
   animate();
 });
