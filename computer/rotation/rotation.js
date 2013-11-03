b0af2e88f70f05f1399baf3bbc12a7fa616e6170
tamubun <sutebubon@gmail.com>
2013-11-03 11:33:59 +0900

    物理的に正しく回るようになった

--- computer/rotation/rotation.js
+++ computer/rotation/rotation.js
@@ -2,15 +2,18 @@
 var camera, scene, renderer, controls;
 
 var radius, height, theta, mom;
+var I1, I2, I3;
 var cylinder;
 
 function newSettings() {
   radius = Number($('#radius').val());
   height = Number($('#height').val());
-  theta = Number($('#theta').val());
+  theta = Math.PI/180.0 * Number($('#theta').val());
   mom = Number($('#mom').val());
   cylinder.scale.set(radius * 50, height * 50, radius * 50);
-  cylinder.rotation.set(0,0,Math.PI/180.0 * theta);
+  cylinder.quaternion.setFromAxisAngle(new THREE.Vector3(-1, 0, 0), theta);
+  I1 = I2 = radius * radius / 4.0 + height * height / 12.0;
+  I3 = radius * radius / 2.0;
 }
 
 function init() {
@@ -18,7 +21,7 @@ function init() {
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(
     45, arena.innerWidth() / arena.innerHeight(), 1, 2000);
-  camera.position.set(447, 400, -665);
+  camera.position.set(0, 100, -665);
   camera.lookAt( scene.position );
   controls = new THREE.TrackballControls(camera, arena[0]);
   controls.rotateSpeed = 1.0;
@@ -43,6 +46,7 @@ function init() {
     new THREE.MeshLambertMaterial({ ambient: 0xbbbbbb, color: 0xff2222 }));
   mark.position.x = 0.5;
   cylinder.add(mark);
+  cylinder.useQuaternion = true;
   newSettings();
   scene.add( cylinder );
 
@@ -52,9 +56,20 @@ function init() {
 }
 
 function animate() {
-  var timer = Date.now() * 0.0002;
   requestAnimationFrame(animate);
-  cylinder.rotation.y = timer * mom;
+  var timer = Date.now() * 0.0004;
+  var omega_phi = mom / I1,
+      phi = timer * omega_phi,
+      psi = -timer * (I3-I1)/I3 * Math.cos(theta) * omega_phi;
+  var q1, q2;
+  q1 = new THREE.Quaternion();
+  q1.setFromAxisAngle(new THREE.Vector3(0, 1, 0), psi);
+  q2 = new THREE.Quaternion();
+  q2.setFromAxisAngle(new THREE.Vector3(-1, 0, 0), theta);
+  q2.multiply(q1);
+  cylinder.quaternion.setFromAxisAngle(new THREE.Vector3(0, 1, 0), phi);
+  cylinder.quaternion.multiply(q2);
+
   controls.update();
   renderer.render(scene, camera);
 }
