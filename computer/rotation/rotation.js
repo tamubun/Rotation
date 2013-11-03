f62a03f23e2b1b172447a75ffc48103e2c78c01c
tamubun <sutebubon@gmail.com>
2013-11-03 18:54:10 +0900

    慣性楕円体と普遍平面表示オプション追加。
    ついでに角運動量表示オプションもあるが、まだ、使えない

--- computer/rotation/rotation.js
+++ computer/rotation/rotation.js
@@ -1,9 +1,9 @@
 'use strict';
 var camera, scene, renderer, controls;
 
-var radius, height, theta, mom;
+var radius, height, theta, mom, omega_phi, omega_psi;
 var I1, I2, I3;
-var cylinder;
+var cylinder, ellipse, plane;
 
 function newSettings() {
   radius = Number($('#radius').val());
@@ -14,6 +14,22 @@ function newSettings() {
   cylinder.quaternion.setFromAxisAngle(new THREE.Vector3(-1, 0, 0), theta);
   I1 = I2 = radius * radius / 4.0 + height * height / 12.0;
   I3 = radius * radius / 2.0;
+  omega_phi = mom / I1;
+  omega_psi = -(I3 - I1)/I3 * Math.cos(theta) * omega_phi;
+  var omega_3 = omega_phi * Math.cos(theta) + omega_psi,
+      A = omega_phi * Math.sin(theta),
+      E = 0.5 * ( I1 * A * A + I3 * omega_3 * omega_3 ),
+      scale = 70;
+  ellipse.scale.set(scale / Math.sqrt(I1), scale / Math.sqrt(I3), scale / Math.sqrt(I1));
+  plane.position.y = ellipse.position.y + scale * Math.sqrt(2 * E) / mom;
+}
+
+function newConfigs() {
+  if ( $('#ellipse').prop('checked') ) {
+    ellipse.visible = plane.visible = true;
+  } else {
+    ellipse.visible = plane.visible = false;
+  }
 }
 
 function init() {
@@ -21,7 +37,7 @@ function init() {
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(
     45, arena.innerWidth() / arena.innerHeight(), 1, 2000);
-  camera.position.set(0, 100, -665);
+  camera.position.set(0, 200, -800);
   camera.lookAt( scene.position );
   controls = new THREE.TrackballControls(camera, arena[0]);
   controls.rotateSpeed = 1.0;
@@ -42,7 +58,8 @@ function init() {
 
   cylinder = new THREE.Mesh(
     new THREE.CylinderGeometry(1, 1, 1, 30, 1),
-    new THREE.MeshLambertMaterial({ ambient: 0xbbbbbb, color: 0x335577 }));
+    new THREE.MeshLambertMaterial(
+      { ambient: 0xbbbbbb, color: 0x335577 }));
   var mark = new THREE.Mesh(
     new THREE.CubeGeometry(1.005, 1.005, 0.1),
     new THREE.MeshLambertMaterial({ ambient: 0xbbbbbb, color: 0xff2222 }));
@@ -50,18 +67,38 @@ function init() {
   cylinder.add(mark);
   cylinder.useQuaternion = true;
   cylinder.castShadow = true;
-  newSettings();
+  cylinder.position.y = 100;
   scene.add( cylinder );
 
   var ground_material =
     new THREE.MeshLambertMaterial({ ambient: 0xbbbbbb, color: 0xaa7744 });
   ground_material.side = THREE.DoubleSide;
   var ground = new THREE.Mesh(
-    new THREE.PlaneGeometry(700, 700), ground_material);
+    new THREE.PlaneGeometry(600, 600), ground_material);
   ground.rotation.x = -Math.PI / 2;
-  ground.position.y = -200;
+  ground.position.y = -80;
   ground.receiveShadow = true;
   scene.add(ground);
+
+  ellipse = new THREE.Mesh(
+    new THREE.SphereGeometry(1, 16, 20),
+    new THREE.MeshLambertMaterial(
+      { ambient: 0xbbbbbb, color: 0xff2222, transparent: true, opacity: 0.2 }));
+  ellipse.position.y = cylinder.position.y;
+  ellipse.quaternion = cylinder.quaternion;
+  scene.add(ellipse);
+
+  var plane_material =
+    new THREE.MeshLambertMaterial(
+      { ambient: 0xbbbbbb, color: 0x777777, transparent: true, opacity: 0.2 });
+  plane_material.side = THREE.DoubleSide;
+  plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(600, 600), plane_material);
+  plane.rotation.x = -Math.PI / 2;
+  scene.add(plane);
+
+  newSettings();
+  newConfigs();
 
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(arena.innerWidth(), arena.innerHeight());
@@ -72,9 +109,8 @@ function init() {
 function animate() {
   requestAnimationFrame(animate);
   var timer = Date.now() * 0.0004;
-  var omega_phi = mom / I1,
-      phi = timer * omega_phi,
-      psi = -timer * (I3-I1)/I3 * Math.cos(theta) * omega_phi;
+  var phi = timer * omega_phi,
+      psi = timer * omega_psi;
   var q1, q2;
   q1 = new THREE.Quaternion();
   q1.setFromAxisAngle(new THREE.Vector3(0, 1, 0), psi);
@@ -90,6 +126,7 @@ function animate() {
 
 $(function() {
   $('#settings input').change(newSettings);
+  $('#configs input').change(newConfigs);
   init();
   animate();
 });
